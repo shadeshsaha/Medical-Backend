9fd1c5cd85121eef56ee364693bbc742f5056c49
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-10-19 17:38:04 +0600

    Added Zod Validation On FAQs

--- src/app/modules/faq/faq.routes.ts
+++ src/app/modules/faq/faq.routes.ts
@@ -2,7 +2,9 @@ import express from 'express';
 
 import { userRole } from '@prisma/client';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { FaqController } from './faq.controller';
+import { FaqValidation } from './faq.validations';
 
 const router = express.Router();
 
@@ -11,12 +13,14 @@ router.get('/', FaqController.getAllFaqs);
 router.post(
   '/create-faq',
   auth(userRole.ADMIN, userRole.SUPER_ADMIN),
+  validateRequest(FaqValidation.createFaq),
   FaqController.createNewFaq
 );
 
 router.patch(
   '/update/:faqId',
   auth(userRole.ADMIN, userRole.SUPER_ADMIN),
+  validateRequest(FaqValidation.updateFaq),
   FaqController.updateFaqDetails
 );
 
